aa9474ba00a03680365a2e463c9a3bd0b2af72d2
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2020-12-08 15:05:16 -0300

    realizado ajuste de formatacao de data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses_flutter_app/models/transactions.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(MyHomeApp());
 
@@ -74,7 +75,7 @@ class ExpensesApp extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: TextStyle(
                               color: Colors.grey[500],
                             ),
