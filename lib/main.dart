b9767096089dd7ad24126f1b86dca532efd31859
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2020-12-08 14:59:21 -0300

    estilizado lista de informacoes de despesas

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,98 @@
+import 'package:expenses_flutter_app/models/transactions.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(MyHomeApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({Key key}) : super(key: key);
+  ExpensesApp({Key key}) : super(key: key);
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis de Corrida',
+      value: 310.20,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 215.40,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Despesas Pessoais"),
-      ),
-      body: Center(
-        child: Text("Despesas Iniciais"),
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Despesas Pessoais"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text("Gráficos "),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          width: 1,
+                          color: Colors.purple,
+                        )),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "R\$ ${tr.value.toStringAsFixed(2)}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                              fontSize: 20),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tr.date.toString(),
+                            style: TextStyle(
+                              color: Colors.grey[500],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
 
 class MyHomeApp extends StatelessWidget {
-  const MyHomeApp({Key key}) : super(key: key);
+  MyHomeApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
