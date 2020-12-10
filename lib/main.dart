f4c1c0fee28fbabd0e8334da5b64bfb23405d87f
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2020-12-10 09:21:19 -0300

    adcionado floating button para adcionar nova transacao

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,33 @@ class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Despesas Pessoais"),
-          actions: [IconButton(icon: Icon(Icons.add), onPressed: () => {})],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text("Gráficos "),
-                  elevation: 5,
-                ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Despesas Pessoais"),
+        actions: [IconButton(icon: Icon(Icons.add), onPressed: () => {})],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text("Gráficos "),
+                elevation: 5,
               ),
-              TransactionUser()
-            ],
-          ),
-        ));
+            ),
+            TransactionUser()
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
 
