d75ca62a74f6694dfc6543ab9482bebf6b361e05
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2020-12-10 09:37:34 -0300

    refatorado adcionar transacao para modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,52 @@
-import 'package:expenses_flutter_app/components/transaction_user.dart';
+import 'dart:math';
+
+import 'package:expenses_flutter_app/components/transaction_form.dart';
+import 'package:expenses_flutter_app/components/transaction_list.dart';
+import 'package:expenses_flutter_app/models/transactions.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(MyHomeApp());
 
-class ExpensesApp extends StatelessWidget {
+class ExpensesApp extends StatefulWidget {
   ExpensesApp({Key key}) : super(key: key);
+
+  @override
+  _ExpensesAppState createState() => _ExpensesAppState();
+}
+
+class _ExpensesAppState extends State<ExpensesApp> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis de Corrida',
+      value: 310.20,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 215.40,
+      date: DateTime.now(),
+    ),
+  ];
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        });
+  }
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() => {_transactions.add(newTransaction)});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +54,11 @@ class ExpensesApp extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text("Despesas Pessoais"),
-        actions: [IconButton(icon: Icon(Icons.add), onPressed: () => {})],
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => _openTransactionFormModal(context))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -26,13 +72,15 @@ class ExpensesApp extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser()
+            TransactionList(
+              transactions: _transactions,
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => {},
+        onPressed: () => _openTransactionFormModal(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
